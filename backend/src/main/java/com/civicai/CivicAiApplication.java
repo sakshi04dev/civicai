83d5413e63bd06238d5e0e3beeeab8e3dafce30d
Sakshi <sashx.004@gmail.com>
2026-09-01 09:26:49 +0530

    Create backend/src/main/java/com/civicai/CivicAiApplication.java

---
+++ backend/src/main/java/com/civicai/CivicAiApplication.java
@@ -0,0 +1,17 @@
+package com.civicai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicAiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                CivicAiApplication.class,
+                args
+        );
+
+    }
+}
